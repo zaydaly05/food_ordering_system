8f5e5016f30b9e54f6501004f6658dd6d1621b8b
omar <omar2301433@miuegypt.edu.eg>
2026-04-30 13:52:12 +0300

    Initial commit

--- mvc/backend/src/main/java/com/foodordering/mvc/notification/NotificationStrategy.java
+++
@@ -1,8 +0,0 @@
-package com.foodordering.mvc.notification;
-
-import com.foodordering.mvc.model.UserAccount;
-
-public interface NotificationStrategy {
-    String send(UserAccount user, String message);
-}
-
