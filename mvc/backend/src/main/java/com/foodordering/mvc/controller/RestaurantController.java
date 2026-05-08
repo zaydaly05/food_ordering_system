7b2a32105b62f6fde27a631e858963974639d89e
mostafa2303547 <mostafa2303547@miuegypt.edu.eg>
2026-05-08 19:23:36 +0300

    Restaurant added update and delete

--- mvc/backend/src/main/java/com/foodordering/mvc/controller/RestaurantController.java
+++ mvc/backend/src/main/java/com/foodordering/mvc/controller/RestaurantController.java
@@ -1,4 +1,5 @@
 package com.foodordering.mvc.controller;
+
 import com.foodordering.mvc.service.RestaurantService;
 import com.foodordering.mvc.model.Restaurant;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,18 +15,36 @@ public class RestaurantController {
     @Autowired
     private RestaurantService service;
 
+    // CREATE
     @PostMapping
     public Restaurant addRestaurant(@RequestBody Restaurant restaurant) {
         return service.addRestaurant(restaurant);
     }
 
+    // READ ALL
     @GetMapping("/all")
     public List<Restaurant> getAllRestaurants() {
         return service.getAllRestaurants();
     }
 
+    // READ BY ID
     @GetMapping("/{id}")
     public Restaurant getRestaurantById(@PathVariable String id) {
         return service.getRestaurantById(id);
+    }
+
+    // UPDATE
+    @PutMapping("/{id}")
+    public Restaurant updateRestaurant(
+            @PathVariable String id,
+            @RequestBody Restaurant restaurant) {
+
+        return service.updateRestaurant(id, restaurant);
+    }
+
+    // DELETE
+    @DeleteMapping("/{id}")
+    public void deleteRestaurant(@PathVariable String id) {
+        service.deleteRestaurant(id);
     }
 }
